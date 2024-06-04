2190d74ba62db5a7a4822ed064a596d3dbc70de8
kaliyappan1 <yappank17@gmail.com>
2024-06-04 20:19:39 +0530

    header style

--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,35 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
+import Logo from './asets/image-removebg-preview (1).png'
+import SearchIcon from '@mui/icons-material/Search';
 
 function Header() {
   return (
-    <div className='header'>
-        <img className='header_logo' src='https://pngimg.com/uploads/amazon/amazon_PNG12.png' />
-        <div className='header_search'>
-            <input className='header_searchInput' type='text'/>
+    <div className="header">
+      <img
+        className="header_logo"
+        src={Logo}
+      />
+      <div className="header_search">
+        <input className="header_searchInput" type="text" />
+        <SearchIcon className="header_searchIcon"/>
+      </div>
+      <div className="header_nav">
+        <div className="header_option">
+          <span className="header_optionLineOne">Hello Guest</span>
+          <span className="header_optionLineTwo">Sign In</span>
         </div>
-        <div className='header_nav'>
-            
+        <div className="header_option">
+          <span className="header_optionLineOne">Returns </span>
+          <span className="header_optionLineTwo">& Orders</span>
         </div>
-
+        <div className="header_option">
+          <span className="header_optionLineOne">Your</span>
+          <span className="header_optionLineTwo">Prime</span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
