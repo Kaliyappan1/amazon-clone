153e49cfdb0237f046c7924db8487eb14e2adad6
kaliyappan1 <yappank17@gmail.com>
2024-06-11 16:13:22 +0530

    cart dynamic subtotal added

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -2,23 +2,35 @@ import React from 'react'
 import './Subtotal.css'
 import { useStateValue} from './StateProvider'
 import { getBasketTotal } from './reducer';
+import CurrencyFormat from 'react-currency-format';
 
 function Subtotal() {
 
   const [{basket}, dispatch] = useStateValue();
 
   return (
+
     <div className='subtotal'>
+      <CurrencyFormat renderText={(value) => (
+        <>
         <p>
-          Subtotal ({basket?.length} items): <strong>{getBasketTotal(basket)}</strong>
+          Subtotal ({basket?.length} items): <strong>{value}</strong>
         </p>
         <small className="subtotal_gift">
-          <input type="checkbox" name="" id="" />
+          <input type="checkbox"/>
           This order containes a gift
         </small>
 
 
-        <button>Proceed to Checkout</button>
+        </>
+      )}
+      decimalScale={2}
+      value={getBasketTotal(basket)}
+      displayType={'text'}
+      thousandSeparator={true}
+      prefix={'$'}
+      />
+    <button>Proceed to Checkout</button>
     </div>
 
   )
