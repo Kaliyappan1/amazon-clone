d749ba5137b04cffbcb30201251a7429fbedae63
kaliyappan1 <yappank17@gmail.com>
2024-06-11 18:33:31 +0530

    product cart

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Checkout.css'
 import Subtotal from './Subtotal.js'
+import CheckoutProduct from './CheckoutProduct.js'
+
 
 function Checkout() {
   return (
@@ -9,12 +11,15 @@ function Checkout() {
             <img className='checkout_ad' src="https://media.gettyimages.com/id/1309469380/vector/bank-insurance-concept-vector-illustration-for-website-banner-advertisement-and-marketing.jpg?s=612x612&w=gi&k=20&c=9Ekan1ZiEHl4BjoidQf_-MBYGq7A7jy-GKQY_nwl4UI=" alt="" />
             <div>
                 <h2 className="checkout_title">Your shopping Basket</h2>
+
+                <CheckoutProduct/>
             </div>
         </div>
         <div className="checkout_right">
             <Subtotal/>
             
         </div>
+        <button>remove from bashket</button>
     </div>
 
   )
