d476cddeb9d947ed36873b9e27d11f3818b4891c
kaliyappan1 <yappank17@gmail.com>
2024-06-14 16:00:23 +0530

    login page completed

--- src/Login.js
+++ src/Login.js
@@ -1,55 +1,77 @@
-import React, { useState } from 'react'
-import './login.css'
-import Loginimg from './asets/image.png'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import './login.css';
+import Loginimg from './asets/image.png';
+import { Link, useNavigate } from 'react-router-dom';
+import { auth } from './firebase';
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 
 function Login() {
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-const signIn = e => {
-  e.preventDefault();
+  const signIn = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        navigate('/');
+      })
+      .catch((error) => alert(error.message));
+  };
 
-  // some fancy firebase login
-}
-
-const register = e => {
-  e.preventDefault();
-
-  // do some fancy firebase register
-}
+  const register = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        if (userCredential) {
+          navigate('/');
+        }
+      })
+      .catch((error) => alert(error.message));
+  };
 
   return (
     <div className='login'>
       <Link to='/'>
-        <img className='login_logo' src={Loginimg} alt="" />
+        <img className='login_logo' src={Loginimg} alt="Amazon Logo" />
       </Link>
 
       <div className="login_container">
         <h1>Sign-in</h1>
-
-        <form >
+        <form>
           <h5>E-mail</h5>
-          <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
 
           <h5>Password</h5>
-          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
 
-          <button type='submit' onClick={signIn} className='login_sinInButton'>Signin</button>
+          <button type='submit' onClick={signIn} className='login_signInButton'>
+            Sign In
+          </button>
         </form>
 
         <p>
-          By signing-in you agree to the AMAZON FAKE CLONE 
-          Condition of Use & Sale. Please see our Privacy Notice, 
-          Our cookies Notice and our Interest-Based Ads Notice.
+          By signing-in you agree to the AMAZON FAKE CLONE
+          Condition of Use & Sale. Please see our Privacy Notice,
+          Our Cookies Notice and our Interest-Based Ads Notice.
         </p>
 
-        <button onClick={register} className='login_registerButton'>Create your amazon account</button>
+        <button onClick={register} className='login_registerButton'>
+          Create your Amazon account
+        </button>
       </div>
     </div>
-
-
-  )
+  );
 }
 
-export default Login
+export default Login;
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,20 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDl2MZq68P9nkI0CUHAWhDQI2QzrkaA1kA",
-    authDomain: "amzon-clone-1.firebaseapp.com",
-    projectId: "amzon-clone-1",
-    storageBucket: "amzon-clone-1.appspot.com",
-    messagingSenderId: "779827790274",
-    appId: "1:779827790274:web:8e81e59fd855a5ba6e565b",
-    measurementId: "G-R8DN2WY4RJ"
-  };
+  apiKey: "AIzaSyDl2MZq68P9nkI0CUHAWhDQI2QzrkaA1kA",
+  authDomain: "amzon-clone-1.firebaseapp.com",
+  projectId: "amzon-clone-1",
+  storageBucket: "amzon-clone-1.appspot.com",
+  messagingSenderId: "779827790274",
+  appId: "1:779827790274:web:8e81e59fd855a5ba6e565b",
+  measurementId: "G-R8DN2WY4RJ"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
