5f6cf1633ee665aea63e39e641c825f105f10f42
kaliyappan1 <yappank17@gmail.com>
2024-06-15 22:39:19 +0530

    checkout page user login add

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,13 +5,14 @@ import CheckoutProduct from "./CheckoutProduct.js";
 import { useStateValue } from "./StateProvider.js";
 
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
 
   return (
     <div className="checkout">
       <div className="checkout_left">
         
         <div>
+          <h3>Hello {user?.email}</h3>
           <h2 className="checkout_title">Your shopping Basket</h2>
 
           {basket.map(item => (
