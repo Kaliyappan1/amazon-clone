60d2a991afe6fc0c3c7cbf4f0a0def3f6d1ab450
kaliyappan1 <yappank17@gmail.com>
2024-06-17 22:06:23 +0530

    add some product size

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Checkout.css";
 import Subtotal from "./Subtotal.js";
+import checkoutbanner from './asets/image copy 2.png'
 import CheckoutProduct from "./CheckoutProduct.js";
 import { useStateValue } from "./StateProvider.js";
 
@@ -10,8 +11,9 @@ function Checkout() {
   return (
     <div className="checkout">
       <div className="checkout_left">
-        
+        <img className="checkout_ad" src={checkoutbanner} alt="" />
         <div>
+
           <h3>Hello {user?.email}</h3>
           <h2 className="checkout_title">Your shopping Basket</h2>
 
