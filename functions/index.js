b97008acdf2b1107a8b13194bf279a8ffcb4c730
kaliyappan1 <yappank17@gmail.com>
2024-06-19 16:13:50 +0530

    firebase app integration

---
+++ functions/index.js
@@ -0,0 +1,7 @@
+
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+const functions = require('firebase-functions');
+const express = require('express');
+
