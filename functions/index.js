42f9ff598fda1962ccb6284e7228d128f2d234c6
kaliyappan1 <yappank17@gmail.com>
2024-06-19 18:52:11 +0530

    paymen firebasea

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,42 @@
-
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
-const functions = require('firebase-functions');
 const express = require('express');
+const cors = require('cors');
+const stripe = require('stripe')('sk_test_51PT1TfRxuVmyxJXTzippUU6WocxxQ25i4DtXj2zYzVnult1zcXGMHelFqkShoGHb6qdrabUA8MA0jCFdhTObezmZ00N17gwaLX');
 
+// API
+
+// App config 
+const app = express();
+
+// Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// API routes
+app.get('/', (request, response) => {
+  response.status(200).send('hello world');
+});
+
+app.post('/payments/create', async (request, response) => {
+    const total = request.query.total;
+
+    console.log('payment Request Recieved Boom!!! for this amount >>>', total);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total, //subunits of the currency 
+        currency: 'usd',
+    });
+
+    // ok - created
+    response.status(201).send({
+        clientSecret:paymentIntent.client_secret,
+    })
+})
+
+// Listen command
+exports.api = onRequest(app);
+
+// example endpoint
+// http://127.0.0.1:5001/amzon-clone-1/us-central1/api
